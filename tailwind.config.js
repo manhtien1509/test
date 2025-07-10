c5b4f7c31c7f752f9d467f75e9730c2633914610
Manh Tien <manhtien1509@gmail.com>
2025-07-10 21:23:05 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // tuỳ chỉnh đường dẫn theo dự án của bạn
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
